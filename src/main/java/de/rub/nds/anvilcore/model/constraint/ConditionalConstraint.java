1d07da12e76d6fd1a2a1d8894e7a46285219e118
Jonas <jonasthi97@gmail.com>
2022-07-08 15:56:12 +0200

    Add ConditionalConstraint

---
+++ src/main/java/de/rub/nds/anvilcore/model/constraint/ConditionalConstraint.java
@@ -0,0 +1,38 @@
+package de.rub.nds.anvilcore.model.constraint;
+
+import de.rub.nds.anvilcore.context.TestContext;
+import de.rub.nds.anvilcore.model.DerivationScope;
+import de.rub.nds.anvilcore.model.parameter.DerivationParameter;
+import de.rub.nds.anvilcore.model.parameter.ParameterIdentifier;
+import de.rwth.swc.coffee4j.model.constraints.Constraint;
+
+import java.util.List;
+import java.util.Set;
+
+public class ConditionalConstraint {
+    private final Set<ParameterIdentifier> requiredParameters;
+    private final Constraint constraint;
+
+    public Set<ParameterIdentifier> getRequiredParameters() {
+        return requiredParameters;
+    }
+
+    public Constraint getConstraint() {
+        return constraint;
+    }
+
+    public boolean isApplicableTo(List<ParameterIdentifier> modeledParameters, DerivationScope derivationScope) {
+        for (ParameterIdentifier required: requiredParameters) {
+            DerivationParameter parameterInstance = TestContext.getInstance().getParameterFactory(required.getParameterType()).getInstance(required);
+            if (!modeledParameters.contains(required) || !parameterInstance.canBeModeled(derivationScope)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public ConditionalConstraint(Set<ParameterIdentifier> requiredParameters, Constraint constraint) {
+        this.requiredParameters = requiredParameters;
+        this.constraint = constraint;
+    }
+}
--- src/main/java/de/rub/nds/anvilcore/model/parameter/ParameterFactory.java
+++ src/main/java/de/rub/nds/anvilcore/model/parameter/ParameterFactory.java
@@ -1,5 +1,7 @@
 package de.rub.nds.anvilcore.model.parameter;
 
 public abstract class ParameterFactory {
+    public abstract DerivationParameter getInstance(ParameterIdentifier parameterIdentifier);
+
     public abstract ParameterScope resolveParameterScope(String scope);
 }
