b87154bdd10a49f742248161a7e9bb77f274f865
Jonas <jonasthi97@gmail.com>
2022-07-14 10:15:00 +0200

    Add resolveParameterCombination helper methods

--- src/main/java/de/rub/nds/anvilcore/junit/CombinatorialAnvilTest.java
+++ src/main/java/de/rub/nds/anvilcore/junit/CombinatorialAnvilTest.java
@@ -1,21 +1,30 @@
 package de.rub.nds.anvilcore.junit;
 
+import de.rub.nds.anvilcore.model.DerivationScope;
 import de.rub.nds.anvilcore.model.ParameterCombination;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 
 @ExtendWith(ExtensionContextParameterResolver.class)
 public abstract class CombinatorialAnvilTest {
     protected static final Logger LOGGER = LogManager.getLogger();
 
     protected ExtensionContext extensionContext;
-    protected ParameterCombination parameterCombination;
 
     @BeforeEach
     public void setExtensionContext(ExtensionContext extensionContext) {
         this.extensionContext = extensionContext;
+    }
+
+    protected ParameterCombination resolveParameterCombination(ArgumentsAccessor argumentsAccessor) {
+        return ParameterCombination.fromArgumentsAccessor(argumentsAccessor, new DerivationScope(extensionContext));
+    }
+
+    protected ParameterCombination resolveParameterCombination(ArgumentsAccessor argumentsAccessor, DerivationScope derivationScope) {
+        return ParameterCombination.fromArgumentsAccessor(argumentsAccessor, derivationScope);
     }
 }
