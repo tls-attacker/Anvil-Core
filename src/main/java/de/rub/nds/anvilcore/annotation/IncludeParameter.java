c2044206aa89d8f5fd6ffd0407cd1e25871c7dac
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2022-10-18 14:11:16 +0200

    feat(model): Add repeatable annotations for including/excluding params
    This adds repeatable annotations to include parameters in or exclude
    parameters from the IPM.
    
    The legacy method using the `IpmExtensions`/`IpmLimitations` has been
    left in place for now.

---
+++ src/main/java/de/rub/nds/anvilcore/annotation/IncludeParameter.java
@@ -0,0 +1,33 @@
+/**
+ * Anvil-Core - A framework for modeling tests
+ *
+ * Copyright 2022 Ruhr University Bochum
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.rub.nds.anvilcore.annotation;
+
+import de.rub.nds.anvilcore.model.parameter.ParameterType;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Includes a parameter in the IPM.
+ *
+ * Repeatable alternative to {@link IpmExtensions}.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Repeatable(IncludeParameters.class)
+public @interface IncludeParameter {
+    String value();
+};
