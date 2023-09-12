19ec01217fbd93c475b53435bb303711484d7175
mmaehren <32199075+mmaehren@users.noreply.github.com>
2023-09-12 13:42:22 +0200

    Deleted leftover semicolon

--- src/main/java/de/rub/nds/anvilcore/execution/AnvilListener.java
+++ src/main/java/de/rub/nds/anvilcore/execution/AnvilListener.java
@@ -11,7 +11,6 @@ public interface AnvilListener {
     default void gotConfig(AnvilTestConfig anvilConfig, String additionalConfig) {}
 
     default void onAborted() {}
-    ;
 
     default boolean beforeStart(TestPlan testPlan, long totalTests) {
         return true;
